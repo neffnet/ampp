fff62118401d49213ed38586ee722e1ce8ef703d
neffnet <neff@neffnet.net>
2015-06-16 13:40:37 -0500

    user can create new site

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,12 +3,31 @@ class SitesController < ApplicationController
     @site = Site.find(params[:id])
   end
 
+  def new
+    @site = current_user.sites.build()
+  end
+
   def create
+    @site = current_user.sites.build(site_params)
+
+    if @site.save
+      flash[:notice] = "New site created!"
+      redirect_to @site
+    else
+      flash[:error] = "Something went wrong"
+      render :create
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  protected
+
+  def site_params
+    params.require(:site).permit(:name)
   end
 end
