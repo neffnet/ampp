2eb98297bd84f027cf09f878f8c00ae2d9c6cf46
neffnet <neff@neffnet.net>
2015-06-30 16:45:46 -0500

    user can add sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,27 @@
 class SitesController < ApplicationController
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = current_user.sites.build(site_params)
+
+    if @site.save
+      flash[:notice] = "#{@site.name} site created!" 
+      redirect_to @site
+    else
+      flash[:error] = @site.errors
+      render :new
+    end
+  end
+
+  def show
+    @site = Site.find(params[:id])
+  end
+
+  protected
+
+  def site_params
+    params.require(:site).permit(:name, :facebook_page_id)
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if user_signed_in?
+      @sites = current_user.sites
+    end
   end
 end
