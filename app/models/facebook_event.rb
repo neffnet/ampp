ace1a17e84b8e0494399b8b733602464275541db
neffnet <neff@neffnet.net>
2015-07-06 16:02:40 -0500

    get all the data with one graph api request

--- app/models/facebook_event.rb
+++ app/models/facebook_event.rb
@@ -1,5 +1,4 @@
 class FacebookEvent
-  # require 'net/http'
   include Facebook
   attr_reader :id, :picture, :name, :venue, :address, :date, :time, :description, :link
 
@@ -7,26 +6,22 @@ class FacebookEvent
     @id = facebook_event_id
 
     FbGraph2.api_version = 'v2.3'
-    event = FbGraph2::Event.new(@id, access_token: facebook_app_access_token, fields: ['cover', 'picture']).fetch
+    event = FbGraph2::Event.new(@id, access_token: facebook_app_access_token).fetch(fields: 'name,description,place,picture')
     @description = event.description
     @name = event.name
-    @venue = event.owner.name
-    @address = get_address(event.owner.id)
+    @venue = event.raw_attributes['place']['name']
+    @address = get_address(event.raw_attributes['place'])
     @date = get_date(event.start_time)
     @time = get_time(event.start_time)
     @link = 'https://facebook.com/' + event.id
-
-    #get picture source URL
-    picture = HTTParty.get("https://graph.facebook.com/v2.3/#{@id}/picture?access_token=#{facebook_app_access_token}&type=large&redirect=false")
-    @picture = picture['data']['url']
+    @picture = event.picture.url
   end
 
   private
 
-  def get_address(venue_id)
-    address = HTTParty.get("https://graph.facebook.com/v2.3/#{venue_id}?access_token=#{facebook_app_access_token}&fields=location")
-    if address['location']
-      "#{address['location']['street']} #{address['location']['city']}, #{address['location']['state']} #{address['location']['zip']}"
+  def get_address(place)
+    if place['location']
+      "#{place['location']['street']} #{place['location']['city']}, #{place['location']['state']} #{place['location']['zip']}"
     else
       @venue
     end
