8805e55304a9f4ae364791e91ac8a8f41b848ba4
neffnet <neff@neffnet.net>
2015-07-06 15:13:03 -0500

    all event info loaded from facebook

--- app/models/facebook_event.rb
+++ app/models/facebook_event.rb
@@ -1,7 +1,7 @@
 class FacebookEvent
   # require 'net/http'
   include Facebook
-  attr_reader :id, :picture, :name, :venue, :address, :date, :time, :description
+  attr_reader :id, :picture, :name, :venue, :address, :date, :time, :description, :link
 
   def initialize(facebook_event_id)
     @id = facebook_event_id
@@ -11,34 +11,32 @@ class FacebookEvent
     @description = event.description
     @name = event.name
     @venue = event.owner.name
-    @address = address(event.owner.id)
-    @date = date(event.start_time)
-    @time = time(event.start_time)
+    @address = get_address(event.owner.id)
+    @date = get_date(event.start_time)
+    @time = get_time(event.start_time)
+    @link = 'https://facebook.com/' + event.id
 
     #get picture source URL
-    # first try - undefined method split for access_token:symbol
-
-    # url = URI.parse("https://graph.facebook.com/v2.3/#{@id}/picture")
-    # params = {access_token: facebook_app_access_token, type: 'large'}
-    # http = Net::HTTP.new('graph.facebook.com', url.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    # @picture = http.get(url, params)
-
-    # try it with httparty gem
-    response = HTTParty.get("https://graph.facebook.com/v2.3/#{@id}/picture?access_token=#{facebook_app_access_token}&type=large")
-    @picture = response.headers.location
+    picture = HTTParty.get("https://graph.facebook.com/v2.3/#{@id}/picture?access_token=#{facebook_app_access_token}&type=large&redirect=false")
+    @picture = picture['data']['url']
   end
 
-  def address(venue_id)
+  private
 
+  def get_address(venue_id)
+    address = HTTParty.get("https://graph.facebook.com/v2.3/#{venue_id}?access_token=#{facebook_app_access_token}&fields=location")
+    if address['location']
+      "#{address['location']['street']} #{address['location']['city']}, #{address['location']['state']} #{address['location']['zip']}"
+    else
+      @venue
+    end
   end
 
-  def date(timestring)
-
+  def get_date(timestring)
+    timestring.strftime('%B %e, %Y')
   end
 
-  def time(timestring)
-
+  def get_time(timestring)
+    timestring.strftime('%l:%M %p')
   end
 end
